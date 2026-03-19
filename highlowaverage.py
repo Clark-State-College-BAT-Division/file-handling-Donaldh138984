2122935519f746561e81bfca454b8c4bf99bcadd
Donaldh138984 <hollad3338@clarkstate.edu>
2026-03-18 21:47:15 -0400

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,31 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+#Count: 51
+#Total: 391
+#Average: 7.88
+#Highest: 469
+#Lowest: 3
+
+curfile = open("numbers.txt")
+
+total = 391
+count = 51
+highest = 469
+lowest = 3
+
+for curline in curfile:
+    total = total + int(curline)
+    count = count + 1
+    if int(curline) > highest:
+        highest = int(curline)
+    if int(curline) < lowest:
+        lowest = int(curline)
+
+average = total / count 
+
+print("Count:", count)
+print("Total:", total)
+print("Average:", average)
+print("Highest:", highest)
+print("Lowest:", lowest)
